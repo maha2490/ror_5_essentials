2b79b5803b1f3c7a14a5c9344bc369db63c74209
Rachel Mahan <rachel.mahan@rtslabs.com>
2017-04-30 13:21:42 -0400

    naming notes + updates

---
+++ app/models/admin_user.rb
@@ -0,0 +1,6 @@
+class AdminUser < ApplicationRecord
+  #when changing a model's table name, you need to either:
+  # 1. update the class method and file name to match. done in this file.
+  # 2. define what table to use as:
+  # self.table_name = "admin_users"
+end
