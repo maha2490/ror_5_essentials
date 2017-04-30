80aa2b9510eac74b727aff40810f5c0fc7682cba
Rachel Mahan <rachel.mahan@rtslabs.com>
2017-04-30 14:51:21 -0400

    added in scopes

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,17 @@
 class Subject < ApplicationRecord
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order("postition ASC") }
+  scope :newest_first, lambda { order("created_at DESC")}
+  scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"])}
+
+  # other way to write scopes
+  # scope :visible, -> { where(:visible => true) }
+
+  # defining scopes are the same as:
+  # def self.active
+  #   where(:active => true)
+  # end
+
+  #lambdas are the correct way to achieve this
 end
